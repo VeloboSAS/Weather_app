c293e8e7ecc8fe2fdd5874bfb0be263d8e708bbe
Соболев Алексей <alexss81@mail.ru>
2020-11-01 17:20:35 +0300

    modified app.py

--- app.py
+++ app.py
@@ -15,14 +15,15 @@ def get_weather(city):
     result = requests.get(url.format(city, api_key))
     if result:
         json = result.json()
-        # (City, Country, temp_celsius, icon, weather)
+        # (City, Country, temp_celsius, temp_fahrenheit, icon, weather)
         city = json['name']
         country = json['sys']['country']
         temp_kelvin = json['main']['temp']
         temp_celsius = temp_kelvin - 273.15
+        temp_fahrenheit = (temp_kelvin - 273.15) * 9 / 5 + 2
         icon = json['weather'][0]['icon']
         weather = json['weather'][0]['main']
-        final = (city, country, temp_celsius, icon, weather)
+        final = (city, country, temp_celsius, temp_fahrenheit, icon, weather)
         return final
     else:
         return None
@@ -33,16 +34,16 @@ def search():
     weather = get_weather(city)
     if weather:
         location_lbl['text'] = '{}, {}'.format(weather[0], weather[1])
-        image['bitmap'] = 'weather_icons/{}.png'.format(weather[3])
-        temp_lbl['text'] = '{:.2f}C'.format(weather[2])
-        weather_lbl['text'] = weather[4]
+        image['bitmap'] = 'weather_icons/{}.png'.format(weather[4])
+        temp_lbl['text'] = '{:.2f}°C', '{:.2f}°F'.format(weather[2], weather[3])
+        weather_lbl['text'] = weather[5]
     else:
         messagebox.showerror('Error', 'Cannot find city {}'.format(city))
 
 
 app = Tk()
 app.title('Weather app')
-app.geometry('700x350')
+app.geometry('500x300')
 
 city_text = StringVar()
 city_entry = Entry(app, textvariable=city_text)
@@ -51,7 +52,7 @@ city_entry.pack()
 search_btn = Button(app, text='Search weather', width=12, command=search)
 search_btn.pack()
 
-location_lbl = Label(app, text='', font=('bold', 20))
+location_lbl = Label(app, text='', font=('bold', 40))
 location_lbl.pack()
 
 image = Label(app, bitmap='')
